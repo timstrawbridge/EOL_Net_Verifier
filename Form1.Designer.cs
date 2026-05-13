1038ca516b288f984cc104b5885b041fcb0ebc26
Tim.Strawbridge <mixmaster1413@gmail.com>
2026-05-13 15:56:30 -0500

    Added list adapter

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,40 +28,42 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
+            this.txtStart = new System.Windows.Forms.TextBox();
+            this.btnScan = new System.Windows.Forms.Button();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.txtEnd = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.lstAdapters = new System.Windows.Forms.ListView();
             this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
-            // textBox1
+            // txtStart
             // 
-            this.textBox1.Location = new System.Drawing.Point(17, 45);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
-            this.textBox1.TabIndex = 0;
+            this.txtStart.Location = new System.Drawing.Point(17, 45);
+            this.txtStart.Name = "txtStart";
+            this.txtStart.Size = new System.Drawing.Size(100, 20);
+            this.txtStart.TabIndex = 0;
             // 
-            // button1
+            // btnScan
             // 
-            this.button1.Location = new System.Drawing.Point(258, 43);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "Scan";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.btnScan.Location = new System.Drawing.Point(258, 43);
+            this.btnScan.Name = "btnScan";
+            this.btnScan.Size = new System.Drawing.Size(75, 23);
+            this.btnScan.TabIndex = 1;
+            this.btnScan.Text = "Scan";
+            this.btnScan.UseVisualStyleBackColor = true;
+            this.btnScan.Click += new System.EventHandler(this.button1_Click);
             // 
             // groupBox1
             // 
             this.groupBox1.Controls.Add(this.label2);
-            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.txtEnd);
             this.groupBox1.Controls.Add(this.label1);
-            this.groupBox1.Controls.Add(this.textBox1);
-            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.txtStart);
+            this.groupBox1.Controls.Add(this.btnScan);
             this.groupBox1.Location = new System.Drawing.Point(418, 12);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Size = new System.Drawing.Size(377, 107);
@@ -69,39 +71,49 @@
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Ping Range";
             // 
-            // groupBox2
+            // label2
             // 
-            this.groupBox2.Location = new System.Drawing.Point(12, 8);
-            this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(377, 111);
-            this.groupBox2.TabIndex = 3;
-            this.groupBox2.TabStop = false;
-            this.groupBox2.Text = "Network Adapters";
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(135, 28);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "End";
+            // 
+            // txtEnd
+            // 
+            this.txtEnd.Location = new System.Drawing.Point(138, 46);
+            this.txtEnd.Name = "txtEnd";
+            this.txtEnd.Size = new System.Drawing.Size(100, 20);
+            this.txtEnd.TabIndex = 3;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(14, 27);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(32, 15);
+            this.label1.Size = new System.Drawing.Size(29, 13);
             this.label1.TabIndex = 2;
             this.label1.Text = "Start";
             // 
-            // label2
+            // groupBox2
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(135, 28);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(29, 15);
-            this.label2.TabIndex = 4;
-            this.label2.Text = "End";
+            this.groupBox2.Controls.Add(this.lstAdapters);
+            this.groupBox2.Location = new System.Drawing.Point(12, 8);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(377, 265);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Network Adapters";
             // 
-            // textBox2
+            // lstAdapters
             // 
-            this.textBox2.Location = new System.Drawing.Point(138, 46);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 20);
-            this.textBox2.TabIndex = 3;
+            this.lstAdapters.HideSelection = false;
+            this.lstAdapters.Location = new System.Drawing.Point(17, 32);
+            this.lstAdapters.Name = "lstAdapters";
+            this.lstAdapters.Size = new System.Drawing.Size(340, 209);
+            this.lstAdapters.TabIndex = 0;
+            this.lstAdapters.UseCompatibleStateImageBehavior = false;
             // 
             // Form1
             // 
@@ -112,21 +124,24 @@
             this.Controls.Add(this.groupBox1);
             this.Name = "Form1";
             this.Text = "EOL Network Verifier";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtStart;
+        private System.Windows.Forms.Button btnScan;
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox txtEnd;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ListView lstAdapters;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
 
 namespace EOL_Net_Verifier
 {
@@ -20,6 +22,58 @@ namespace EOL_Net_Verifier
         private void button1_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // configure list view for details and columns
+            lstAdapters.View = View.Details;
+            lstAdapters.FullRowSelect = true;
+            lstAdapters.GridLines = true;
+            lstAdapters.Columns.Clear();
+            lstAdapters.Columns.Add("Name", 120);
+            lstAdapters.Columns.Add("Description", 220);
+            lstAdapters.Columns.Add("Status", 90);
+            lstAdapters.Columns.Add("MAC", 140);
+            lstAdapters.Columns.Add("IP Addresses", 200);
+
+            lstAdapters.Items.Clear();
+
+            try
+            {
+                foreach (var ni in NetworkInterface.GetAllNetworkInterfaces())
+                {
+                    var ipProps = ni.GetIPProperties();
+                    var ips = ipProps.UnicastAddresses
+                        .Where(u => u.Address.AddressFamily == AddressFamily.InterNetwork)
+                        .Select(u => u.Address.ToString());
+
+                    string ipList = string.Join(", ", ips);
+
+                    var macBytes = ni.GetPhysicalAddress().GetAddressBytes();
+                    string mac = macBytes.Length == 0 ? string.Empty : BitConverter.ToString(macBytes).Replace("-", ":");
+
+                    var item = new ListViewItem(new[] {
+                        ni.Name,
+                        ni.Description,
+                        ni.OperationalStatus.ToString(),
+                        mac,
+                        ipList
+                    });
+
+                    lstAdapters.Items.Add(item);
+                }
+
+                // auto-size columns to content
+                for (int i = 0; i < lstAdapters.Columns.Count; i++)
+                {
+                    lstAdapters.AutoResizeColumn(i, ColumnHeaderAutoResizeStyle.ColumnContent);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Failed to enumerate network adapters: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
     }
 }
